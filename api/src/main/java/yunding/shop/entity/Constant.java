2ec6b7f26e1e67bf2a31f505f46beb7fbfa2be0d
qiyubing <1248288510@qq.com>
2018-08-03 21:16:11 +0800

    qyb

--- api/src/main/java/yunding/shop/entity/Constant.java
+++ api/src/main/java/yunding/shop/entity/Constant.java
@@ -6,10 +6,29 @@ package yunding.shop.entity;
  * @author guo
  */
 public class Constant {
+    /**
+     * 提示最大条数
+     */
     public static final Integer HINT_SIZE = 5;
+
+    /**
+     * 订单 待付款状态
+     */
     public static final Integer WAIT_PAY = 0;
+    /**
+     * 订单 待发货状态
+     */
     public static final Integer WAIT_SEND_GOOD = 1 ;
+    /**
+     * 订单 待收货状态
+     */
     public static final Integer WAIT_RECEIVE_GOOD = 2;
+    /**
+     * 订单 待评价状态
+     */
     public static final Integer WAIT_COMMENT = 3;
+    /**
+     * 订单 结束
+     */
     public static final Integer OVER_ORDER = 4;
 }
