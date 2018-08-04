694ae6b9ee875a3840af93a6bd63fb94703c115c
qiyubing <1248288510@qq.com>
2018-08-04 23:50:54 +0800

    qyb

--- api/src/test/java/Service.java
+++
@@ -1,5 +0,0 @@
-public class Service {
-    public void setId(Goods goods){
-        goods.setId(1);
-    }
-}
--- api/src/test/java/Test.java
+++ api/src/test/java/Test.java
@@ -6,9 +6,6 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        Goods goods = new Goods();
-        Service service = new Service();
-        service.setId(goods);
-        System.out.println(goods.getId());
+
     }
 }
